aff8db42628bcad9afc5b987b909fbdfda25b112
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-12-04 21:38:08 +0000

    added gitpod.io to allowed host

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = 'django-insecure-!4b798*#i-*7m&r37et9=7xu!d1yop$c5i)=kbhb#$h90*7-@7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.gitpod.io', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
