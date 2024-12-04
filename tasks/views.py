0a1cae158574e83637295cebb6197b44daae2057
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-12-04 20:49:48 +0000

    added create_task

--- tasks/views.py
+++ tasks/views.py
@@ -7,3 +7,7 @@ from .models import Task
 def task_list(request):
     tasks = Task.objects.filter(user=request.user)
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+@login_required
+def create_task(request):
+    
