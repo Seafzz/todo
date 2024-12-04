0de0ac61408d19f2566fbf6c86454be47a260ec8
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-12-04 21:53:40 +0000

    added detail url pattern

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,4 +6,5 @@ urlpatterns =[
     path('create/', views.create_task, name='create_task'),
     path('edit/<int:pk>/', views.edit_task, name='edit_task'),
     path('delete/<int:pk>/', views.delete_task, name='delete_task'),
+    path('detail/<int:pk>/', views.task_detail, name='task_detail'),
 ]
