8877c58201a4d0a4e9139d8b88e4b9829c58fff4
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-12-04 20:46:26 +0000

    added task_list

--- tasks/views.py
+++ tasks/views.py
@@ -2,3 +2,7 @@ from djanog.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import TaskForm
 from .models import Task
+
+@login_required
+def task_list(request):
+    
