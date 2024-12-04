81172888ad3adafef2a020812efa25c5972f0f6a
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-12-04 21:02:35 +0000

    update: updated url paths with a missing colon

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns =[
     path('', views.task_list, name='task_list'),
     path('create/', views.create_task, name='create_task'),
-    path('edit/<int:pk>/', views.edit_task, name='edit_task')
+    path('edit/<int:pk>/', views.edit_task, name='edit_task'),
     path('delete/<int:pk>/', views.delete_task, name='delete_task'),
 ]
