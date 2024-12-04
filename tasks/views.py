58fa63e33b48ffe91f82f774ac7bc6c275a79951
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-12-04 21:30:14 +0000

    fixed a spelling error in views.py

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from djanog.shortcuts import render, redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import TaskForm
 from .models import Task
