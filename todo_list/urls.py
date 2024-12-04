c73fbefbe867451f59a054dbec1fac9ee57170c5
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-12-04 22:45:00 +0000

    added signup url to urls.py

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path ('', include('tasks.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('signup/', task_views.signup, name='signup'),
 ]
