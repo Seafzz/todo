1f233ed2d789aa2f89a45bd7bb48521af60da113
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-12-04 22:02:59 +0000

    fix: fixed multiple small issues and misspels in views.py to make task details to be shown

--- tasks/views.py
+++ tasks/views.py
@@ -8,9 +8,10 @@ def task_list(request):
     tasks = Task.objects.filter(user=request.user)
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
 
+@login_required
 def task_detail(request, pk):
-    tast = get_object_or_404(Task, pk=pk, user=request.user)
-    return render(request, 'tasks/task.detail.html', {'task': task})
+    task = get_object_or_404(Task, pk=pk, user=request.user)
+    return render(request, 'tasks/task_detail.html', {'task': task})
 
 @login_required
 def create_task(request):
@@ -21,30 +22,27 @@ def create_task(request):
             task.user = request.user
             task.save()
             return redirect('task_list')
-
-    else: 
+    else:
         form = TaskForm()
     return render(request, 'tasks/task_form.html', {'form': form})
 
-
 @login_required
-
 def edit_task(request, pk):
-    task = Task.objects.get(pk=pk, user=request.user)
+    task = get_object_or_404(Task, pk=pk, user=request.user)
     if request.method == 'POST':
         form = TaskForm(request.POST, instance=task)
         if form.is_valid():
             form.save()
-        return redirect ('task_list')
+            return redirect('task_list')
     else:
         form = TaskForm(instance=task)
-    return render(request, 'tasks/task_form.html', {'form: form'})
-
+    return render(request, 'tasks/task_form.html', {'form': form})
 
 @login_required
 def delete_task(request, pk):
-    task = Task.objects.get(pk=pk, user=request.user)
+    task = get_object_or_404(Task, pk=pk, user=request.user)
     if request.method == 'POST':
         task.delete()
         return redirect('task_list')
     return render(request, 'tasks/task_confirm_delete.html', {'task': task})
+
