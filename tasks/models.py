c04c4e58d9c7c05ee4aef91f0a348b6ad2ddcf8a
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-12-04 20:40:09 +0000

    fix: fixed small typos

--- tasks/models.py
+++ tasks/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    description = models.textField()
-    completed = models.BooleanField(defualt=False)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
     due_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
-    return self.title
+        return self.title
