b09dd1a5775725b5a55769cb332af8741e4e3b35
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-12-04 20:31:20 +0000

    added task model, and loaded it with user/title/desc/comp/duedate

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,9 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.textField()
+    completed = models.BooleanField(defualt=False)
+    due_date = models.DateField(null=True, blank=True)
