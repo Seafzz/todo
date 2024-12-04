3666b3c7b21b4c8b4d75cfad5049f765ef53d200
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-12-04 21:01:44 +0000

    added url path to edit_task

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns =[
     path('', views.task_list, name='task_list'),
     path('create/', views.create_task, name='create_task'),
+    path('edit/<int:pk>/', views.edit_task, name='edit_task')
 ]
