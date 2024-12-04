c5ad74810784a83cb513fbc2a6bf48cf1a6e8328
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-12-04 20:49:20 +0000

    added render request to task_list

--- tasks/views.py
+++ tasks/views.py
@@ -5,4 +5,5 @@ from .models import Task
 
 @login_required
 def task_list(request):
-    
+    tasks = Task.objects.filter(user=request.user)
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
