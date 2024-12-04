9a3d02ee6f9aa4d1708f5b5d731370aed9c86ea2
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-12-04 22:28:47 +0000

    added signup data for the user to provide

--- tasks/views.py
+++ tasks/views.py
@@ -51,5 +51,9 @@ def signup(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('task_list')
 
