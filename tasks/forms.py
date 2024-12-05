aece970846fb709d365c7584cd3d534bb56a53df
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-12-05 13:09:58 +0000

    imported FormHelper and Submit from crispyForms

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Task
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 class TaskForm(forms.ModelForm):
     class Meta:
