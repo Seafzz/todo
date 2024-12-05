d3c5939f0e624a214ccbc81158f29fa2b75ab7a9
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-12-05 13:45:43 +0000

    fix: fixed a typo changing django.db to django.conf

--- tasks/models.py
+++ tasks/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.db import settings
+from django.conf import settings
 
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
