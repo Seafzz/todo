eaf630ae5449982ebf77ece08bdee87cfb950264
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-12-05 13:33:24 +0000

    added user/bio/location and birth_date to profile

--- tasks/models.py
+++ tasks/models.py
@@ -13,4 +13,7 @@ class Task(models.Model):
         return self.title
 
 class Profile(models.Model):
-    
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
