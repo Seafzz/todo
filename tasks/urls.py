15f1137f4e7f94573d7b7a15cd02253a3c3751ae
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-12-05 14:29:20 +0000

    update path to homepage instead of task_list

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.task_list, name='task_list'),
+    path('', views.task_list, name='home'),
     path('create/', views.create_task, name='create_task'),
     path('edit/<int:pk>/', views.edit_task, name='edit_task'),
     path('delete/<int:pk>/', views.delete_task, name='delete_task'),
