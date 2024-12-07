2764e8d15cd8af5c359b7208c71e24891c887789
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-12-07 01:10:07 +0000

    added priority choices  logic to Task

--- tasks/models.py
+++ tasks/models.py
@@ -19,6 +19,7 @@ class Task(models.Model):
     completed = models.BooleanField(default=False)
     due_date = models.DateField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_ad=True)
+    priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES, default='M')
 
     def __str__(self):
         return self.title
