266563356a9e69461d14a5236343f5256c9408c1
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-12-07 14:42:51 +0000

    formated test.py to meet pep8 standards

--- tasks/tests.py
+++ tasks/tests.py
@@ -6,14 +6,14 @@ from .models import Task
 
 
 class TaskModelTest(TestCase):
-    def setUp (self):
-        self.task = Task.objects.create(title="Test Task", description="This is a test task", completed=False)
+    def setUp(self):
+        self.task = Task.objects.create(
+            title="Test Task", description="This is a test task", completed=False)
 
     def tesk_task_creation(self):
         self.assertEqual(self.task.title, "Test Task")
         self.assertEqual(self.task.description, "This is a test task")
         self.assertEqual(self.task.completed)
-    
+
     def test_string_representation(self):
         self.assert_Equal(str(self.task), self.task.title)
-        
