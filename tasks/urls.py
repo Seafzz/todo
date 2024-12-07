4de139f2d1cac563cac8a7fc9e1e254bd38d8e68
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-12-07 21:42:56 +0000

    update added admin urls to the top of url patterns

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,6 +2,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    # Admin URLs path
+    path('admin/dashboard/', views.admin_dashboard_view, name='admin_dashboard'), 
+    path('admin/manage_tasks/', views.manage_tasks_view, name='manage_tasks'),
+
     path('', views.home, name='home'),
     path('tasks/', views.task_list, name='task_list'),
     path('create/', views.create_task, name='create_task'),
@@ -15,8 +19,4 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('tasks/<int:pk>/toggle_complete/',
          views.toggle_task_complete, name='toggle_task_complete'),
-
- # Admin URLs path
-    path('admin/dashboard/', views.admin_dashboard_view, name='dashboard'), 
-    path('admin/manage_tasks/', views.manage_tasks_view, name='manage_tasks'),
 ]
