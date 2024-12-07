da5dfc9794366e3a9f45b658965c9ceb2a57034a
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-12-07 01:11:08 +0000

    added category choice to task

--- tasks/models.py
+++ tasks/models.py
@@ -20,6 +20,7 @@ class Task(models.Model):
     due_date = models.DateField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_ad=True)
     priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES, default='M')
+    category = models.CharField(max_length=1, choices=CATEGORY_CHOICES, default='O')
 
     def __str__(self):
         return self.title
