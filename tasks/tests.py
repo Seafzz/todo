796b311f1a0882dcfc88cb147bb4b68f53a3f0fe
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-12-07 14:40:48 +0000

    add basic modeltest for task

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Task
 
 # Create your tests here.
+
+
+class TaskModelTest(TestCase):
+    def setUp (self):
+        self.task = Task.objects.create(title="Test Task", description="This is a test task", completed=False)
+
+    def tesk_task_creation(self):
+        self.assertEqual(self.task.title, "Test Task")
+        self assertEqual(self.task.description, "This is a test task")
+        self.assertEqual(self.task.completed)
+    
+    def test_string_representation(self):
+        self.assert_Equal(str(self.task), self.task.title)
+        
