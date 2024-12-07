d4ddc2c1b5e60988d8ddf52815b83c848d752c79
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-12-07 14:47:43 +0000

    add def test_task_list

--- tasks/tests.py
+++ tasks/tests.py
@@ -18,7 +18,14 @@ class TaskModelTest(TestCase):
     def test_string_representation(self):
         self.assert_Equal(str(self.task), self.task.title)
 
+
 class TaskViewTests(TestCase):
     def setup(self):
-        self.task = Task.objects.create(title="Test Task", description="This is a test task", completed=False)
-        
+        self.task = Task.objects.create(
+            title="Test Task", description="This is a test task", completed=False)
+
+    def test_task_list_view(self):
+        response = self.client.get(reverse('task_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assert_Contains(response, "Test Task")
+        self.assertTemplateUsed(respone, 'task/task_list.html')
