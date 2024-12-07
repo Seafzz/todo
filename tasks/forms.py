fcea2ca55b1a061c9ce0ba373e88e58887cf94e3
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-12-07 14:11:10 +0000

    formated forms.py to meet pep8 standards

--- tasks/forms.py
+++ tasks/forms.py
@@ -4,16 +4,20 @@ from .models import Profile
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 
+
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title', 'description', 'due_date', 'priority', 'category', 'completed']
+        fields = ['title', 'description', 'due_date',
+                  'priority', 'category', 'completed']
 
     def __init__(self, *args, **kwargs):
         super(TaskForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = 'post'
-        self.helper.add_input(Submit('submit', 'Save', css_class='btn btn-success'))
+        self.helper.add_input(
+            Submit('submit', 'Save', css_class='btn btn-success'))
+
 
 class ProfileForm(forms.ModelForm):
     class Meta:
